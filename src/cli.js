2c602fde255865bb269829833c05eb6f0862641f
Егор Северов <egorseveroff@gmail.com>
2024-02-11 21:33:17 +0400

    fix comit level 5

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+
 const greetingUser = () => {
   const name = readlineSync.question('May I have your name? ');
   return console.log(`${'Hello, '}${name}${'!'}`);
