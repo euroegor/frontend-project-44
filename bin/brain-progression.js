f5214666a1a05c71293d5053a5a8bda2ddc44a04
Егор Северов <egorseveroff@gmail.com>
2024-04-29 20:41:09 +0400

    common logic

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import GameProgression from '../games/game-progression.js';
+import getMotor from '../games/game-even.js';
 
-GameProgression();
+getMotor();
--- games/game-progression.js
+++ games/game-progression.js
@@ -1,14 +1,13 @@
-import readlineSync from 'readline-sync';
+import { getMotor, ranNum } from '../src/index.js';
 
-const GameProgression = () => {
-  const RanNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`${'Hello, '}${name}${'!'}\n${'What number is missing in the progression?'}`);
+const mission = 'What number is missing in the progression?';
+
+const gameGear = () => {
   const RanProgression = () => {
     const arr1 = [2, 3, 4, 5, 6, 7, 8, 9, 10];
     const arr2 = [];
-    const num1 = arr1[RanNum(0, 8)];
-    for (let i = RanNum(0, 100); i < 1000; i += num1) {
+    const num1 = arr1[ranNum(0, 8)];
+    for (let i = ranNum(0, 100); i < 1000; i += num1) {
       arr2.push(i);
       if (arr2.length === 10) {
         break;
@@ -16,24 +15,14 @@ const GameProgression = () => {
     }
     return arr2;
   };
-  let i = 0;
-  while (i < 3) {
-    const ran = RanProgression();
-    const num3 = RanNum(0, 9);
-    const b = ran[num3];
-    ran[num3] = '..';
-    console.log(`${'Question: '}${ran.join(' ')}`);
-    const name1 = readlineSync.question('Your answer: ');
-    const a = name1;
-    if (Number(name1) === b) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${a}' is wrong answer ;(. Correct answer was '${b}'.`);
-      return console.log(`"Let's try again, ${name}!"`);
-    }
-    i += 1;
-  }
-  return console.log(`${'Congratulations, '}${name}${'!'}`);
+  const ran = RanProgression();
+  const num3 = ranNum(0, 9);
+  const b = String(ran[num3]);
+  ran[num3] = '..';
+  const a = ran.join(' ');
+  return [a, b];
 };
 
-export default GameProgression;
+export default () => {
+  getMotor(mission, gameGear);
+};
