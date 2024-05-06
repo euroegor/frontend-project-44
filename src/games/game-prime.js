ca178f7f9cf426588f80c20cda90baed8edb621a
Егор Северов <egorseveroff@gmail.com>
2024-05-06 17:00:46 +0400

    FINAL FIX!

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -5,6 +5,9 @@ const mission = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const roundDataGeneration = () => {
   const primeTest = (num) => {
+    if (num === 1) {
+      return false;
+    }
     for (let i = 2; i < num; i += 1) {
       if (num % i === 0) {
         return false;
